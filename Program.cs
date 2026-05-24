bc2975c5133abb9287253971970cf8de5944430a
Matheus <matheeuzinhoo282@gmail.com>
2026-05-24 16:26:57 -0300

    https://github.com/fusc/RetContorno.git

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿string continuar = "s";
+while (continuar == "s")
+{
+
+
+
+Console.WriteLine("Tamanho do retângulo"); 
+
+Console.Write("Largura: ");
+int largura = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Altura: "); 
+int altura = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(); 
+
+int contadorlinha = 0;
+while (contadorlinha < altura)
+{
+    int contadorcoluna = 0;
+    while (contadorcoluna < largura)
+    {
+        if (contadorlinha == 0 || contadorlinha == altura - 1 || contadorcoluna == 0 || contadorcoluna == largura - 1)
+        {
+            Console.Write("*");
+        }
+        else
+        {
+            Console.Write(" "); 
+        }
+        
+        contadorcoluna = contadorcoluna + 1;
+    }
+    Console.WriteLine();
+    contadorlinha = contadorlinha + 1;
+}
+Console.Write("Deseja executar novamente? (S/N)?");
+continuar = Console.ReadLine()!.ToLower();
+}
+Console.WriteLine("muito obrigado por usar o programa!");
